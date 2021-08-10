e28d0d90c5b2a6d942c255898d21395d7ebe1fdc
Bruce LIOU <bruceliou@free.fr>
2021-08-10 10:03:36 +0200

    Fix CSS

--- App.js
+++ App.js
@@ -230,7 +230,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#191919",
-    paddingTop: Platform.OS === "android" ? Constants.statusBarHeight : 0,
+    marginTop: Platform.OS === "android" ? Constants.statusBarHeight : 0,
     color: "#ffffff",
   },
   statusBar: {
